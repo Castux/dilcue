6b56c50b6f61df23ae94d778876bddadf9f03ef5
Noé Falzon <noe@yousician.com>
2019-01-30 14:18:20 +0200

    CC intersection

--- geom.lua
+++ geom.lua
@@ -10,6 +10,10 @@ local function pp_squared_distance(p1,p2)
 	return (p1.x - p2.x)^2 + (p1.y - p2.y)^2
 end
 
+local function pp_distance(p1,p2)
+	return math.sqrt(pp_squared_distance(p1,p2))
+end
+
 local function pp_equal(p1,p2)
 	return pp_squared_distance(p1,p2) < eps*eps
 end
@@ -71,12 +75,21 @@ end
 --[[ Circle ]]--
 
 local function Circle(center, p)
-	return { center = center, p = p, sqrad = pp_squared_distance(center, p) }
+	local sqrad = pp_squared_distance(center, p)
+	local rad = math.sqrt(sqrad)
+	
+	return { center = center, p = p, sqrad = sqrad, rad = rad }
 end
 
 local function pc_point_on_circle(p,c)
 
 	return math.abs(pp_squared_distance(c.center, p) - c.sqrad) < eps
+end
+
+
+local function cc_equal(c1,c2)
+	
+	return pp_equal(c1.center, c2.center) and pc_point_on_circle(c1.p, c2)	
 end
 
 local function lc_intersection(l,c)
@@ -112,6 +125,39 @@ local function lc_intersection(l,c)
 			Point((D * dy - tmpx) / dr2 + c.center.x, (-D * dx - tmpy) / dr2 + c.center.y),
 		}
 	end
+end
+
+local function cc_intersection(c1,c2)
+	
+	-- http://paulbourke.net/geometry/circlesphere/
+	
+	local d = pp_distance(c1.center, c2.center)
+	local r0 = c1.rad
+	local r1 = c2.rad
+	
+	if d > r0 + r1 or d < math.abs(r0 - r1) or cc_equal(c1,c2) then
+		return {}
+	end
+	
+	local a = (r0 * r0 - r1 * r1 + d * d) / (2 * d)
+	
+	local px = c1.center.x + a * (c2.center.x - c1.center.x) / d
+	local py = c1.center.y + a * (c2.center.y - c1.center.y) / d
+	
+	local h = math.sqrt(r0 * r0 - a * a)
+	
+	if h < eps then
+		return { Point(px,py) }
+	end
+	
+	local tmpx = h * (c2.center.y - c1.center.y) / d
+	local tmpy = -h * (c2.center.x - c1.center.x) / d
+	
+	return
+	{
+			Point(px + tmpx, py + tmpy),
+			Point(px - tmpx, py - tmpy)
+	}	
 end
 
 local function test()
@@ -164,6 +210,16 @@ local function test()
 	end
 
 	do
+		local c1 = Circle(Point(1,1), Point(0,1))
+		local c2 = Circle(Point(1,1), Point(2,1))
+		local c3 = Circle(Point(0,1), Point(1,1))
+		
+		assert(cc_equal(c1,c1))
+		assert(cc_equal(c1,c2))
+		assert(not cc_equal(c1,c3))
+	end
+
+	do
 		local c = Circle(Point(10,10), Point(10,11))
 		
 		local l = Line(Point(9,12), Point(12,9))
@@ -186,6 +242,36 @@ local function test()
 		local intersections = lc_intersection(l,c)
 		assert(#intersections == 0)
 	end
+	
+	do
+		local c1 = Circle(Point(-2,0), Point(1,0))
+		local c2 = Circle(Point(2,0), Point(1,0))
+		
+		local inter = cc_intersection(c1,c2)
+		assert(#inter == 1)
+		assert(pp_equal(inter[1], Point(1,0)))
+		
+		local c3 = Circle(Point(-1,0), Point(-0.5,0.5))
+		local inter = cc_intersection(c1,c3)
+		assert(#inter == 0)
+	end
+	
+	do
+		local p1 = Point(10,1)
+		local p2 = Point(10,-1)
+		local c1 = Circle(p1,p2)
+		local c2 = Circle(p2,p1)
+		local inter = cc_intersection(c1,c2)
+		assert(#inter == 2)
+		table.sort(inter, function(p1,p2)
+				return p1.x < p2.x
+		end)
+	
+		assert(pp_equal(inter[1], Point(10 - math.sqrt(3), 0)))
+		assert(pp_equal(inter[2], Point(10 + math.sqrt(3), 0)))
+		
+	end
+	
 
 end
 
