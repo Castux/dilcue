fb4631afc5edb191791a8728aefc0118b032e692
Noé Falzon <falzon.noe@gmail.com>
2019-02-01 18:32:01 +0200

    Clarified solver logic

--- solver.lua
+++ solver.lua
@@ -51,12 +51,10 @@ end
 
 local function add_if_unique(object, context)
 
-	
 	local new = true
 	for _,old in ipairs(context.objects) do
 		if geom.equal(object, old) then
 			new = false
-			-- TODO: update
 			break
 		end
 	end
@@ -85,27 +83,63 @@ local function check_solved(context)
 	local num_reached = 0
 	for i,target in ipairs(context.targets) do
 		
-		if target.type == "point" then
-			for _,p in ipairs(context.points) do
-				if geom.equal(p, target) then
-					p.is_target = true
-					num_reached = num_reached + 1
-					break
-				end
-			end
+		local list = target.type == "point" and context.points or context.objects
 		
-		else
-			for _,o in ipairs(context.objects) do
-				if geom.equal(o, target) then
-					o.is_target = true
-					num_reached = num_reached + 1
-					break
-				end
+		for _,o in ipairs(list) do
+			if geom.equal(o, target) then
+				o.is_target = true
+				num_reached = num_reached + 1
+				break
 			end
 		end
 	end
 	
 	return num_reached == #context.targets
+end
+
+local function matches_hint(object, hint)
+	
+	if type(hint) == "table" then
+		return geom.equal(object, hint)
+	
+	elseif type(hint) == "string" then
+		return object.type == hint
+	
+	elseif hint == nil then
+		return true
+		
+	else
+		error("Bad hint")
+	end
+	
+end
+
+local function candidates(context, hint)
+	
+	local tmp = {}
+	
+	for i = 1, #context.points do
+		for j = i+1, #context.points do
+
+			local p1,p2 = context.points[i], context.points[j]
+			table.insert(tmp, geom.Line(p1,p2))
+			table.insert(tmp, geom.Circle(p1,p2))
+			table.insert(tmp, geom.Circle(p2,p1))
+			
+		end
+	end
+	
+	local res = {}
+	
+	-- Filter existing objects and according to hints
+	
+	for _,o in ipairs(tmp) do
+		if matches_hint(o, hint) and not exists(o, context) then
+			table.insert(res, o)
+		end
+	end
+	
+	return res
 end
 
 local function rec(context, depth, max_depth)
@@ -117,79 +151,39 @@ local function rec(context, depth, max_depth)
 		return
 	end
 	
-	-- Make all new possible constructions from existing points
+	-- Make all (uniquely) new possible constructions from existing points
 	
-	local objects = {}
+	local objects = candidates(context, context.hints and context.hints[depth])
 	
-	for i = 1, #context.points do
-		for j = i+1, #context.points do
-
-			local p1,p2 = context.points[i], context.points[j]
-			
-			for i = 1,3 do
-				objects[i] = nil
-			end
-			
-			local line =geom.Line(p1,p2)
-			local c1,c2 = geom.Circle(p1,p2), geom.Circle(p2,p1)
-			
-			local hint = context.hints and context.hints[depth]
-			
-			if not hint then
-				-- No hint, try everything
-				table.insert(objects, line)
-				table.insert(objects, c1)
-				table.insert(objects, c2)
-			
-			elseif type(hint) == "string" then
-			
-				-- Construction type hint
-				if hint == "line" then
-					table.insert(objects, line)
-				elseif hint == "circle" then
-					table.insert(objects, c1)
-					table.insert(objects, c2)
-				else
-					error("Bad hint")
-				end
-				
-			else
-				-- Hint is the actual solution!
-				if geom.equal(hint, line) then
-					table.insert(objects, line)
-				elseif geom.equal(hint, c1) then
-					table.insert(objects, c1)
-				elseif geom.equal(hint, c2) then
-					table.insert(objects, c2)
-				end				
-			end
-			
-			for _,object in ipairs(objects) do
-				
-				local added = add_if_unique(object, context)
-				if added then
-					
-					local num_points_before = #context.points
-					
-					for _,point in ipairs(all_intersections(object, context)) do
-						add_point_if_unique(point, context)
-					end
-					
-					rec(context, depth + 1, max_depth)
-					
-					if context.solved then
-						return
-					end
-					
-					-- backtrack
-					for i = num_points_before + 1, #context.points do
-						context.points[i] = nil
-					end
-					
-					context.objects[#context.objects] = nil
-				end
-			end
+	for _,object in ipairs(objects) do
+		
+		-- Compute intersections
+		
+		local num_points_before = #context.points
+		
+		for _,point in ipairs(all_intersections(object, context)) do
+			add_point_if_unique(point, context)
 		end
+		
+		-- We already insured this object is new, insert directly
+ 		
+		table.insert(context.objects, object)
+		
+		-- Recurse!
+		
+		rec(context, depth + 1, max_depth)
+		
+		if context.solved then
+			return
+		end
+		
+		-- Backtrack
+		
+		for i = num_points_before + 1, #context.points do
+			context.points[i] = nil
+		end
+		
+		context.objects[#context.objects] = nil
 	end
 	
 end
